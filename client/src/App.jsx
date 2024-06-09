189f83dd3cb91927768993411379ad96597fe987
Mohitjaat1 <mcjaat1884@gmail.com>
2024-06-09 11:46:59 +0530

    backend server created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,28 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react'
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Search from './pages/Search';
+import Projects from './pages/Projects';
+import Dashboard from './pages/Dashboard';
+
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Header />
+    <Routes>
+    <Route path='/' element={<Home />} />
+    <Route path='/about' element={<About />} />
+    <Route path='/sign-in' element={<SignIn />} />
+    <Route path='/sign-up' element={<SignUp />} />
+    <Route path='/search' element={<Search />} />
+    <Route path='/dashboard' element={<Dashboard />} />
+    <Route path='/projects' element={<Projects />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
