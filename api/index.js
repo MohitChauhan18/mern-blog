a6896cb783158502d1c6ba8357fa51a8fa16f4c9
Mohitjaat1 <mcjaat1884@gmail.com>
2024-06-11 17:05:26 +0530

    signup created

--- api/index.js
+++ api/index.js
@@ -1,12 +1,21 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth.route.js';
+import userRoutes from './routes/user.route.js';
+
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{console.log("MONGODB CONNECTED")});
 
 const app = express();
+
+app.use(express.json());
+
+app.use('/api/user', userRoutes);
+app.use('/api/auth',authRoutes);
+
 
 app.listen(3000,()=>{
     console.log('Running at port 3000 !');
